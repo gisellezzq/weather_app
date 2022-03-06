1d650e43800a428a1c801d9c6fc37bec7796e2f2
Giselle Zhu <gisellezhu@Giselles-Air.cable.rcn.com>
2022-03-05 16:26:56 -0800

    heroku setup

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+
+const port = process.env.PORT || 3000
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewpath = path.join(__dirname,'../template/views')
@@ -106,6 +108,6 @@ app.get('*', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('server is up')
+app.listen(port, () => {
+    console.log('server is up ' + port)
 })
